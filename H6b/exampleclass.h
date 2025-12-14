368fad7c431e123e026ce8705d8654c01b73ad0d
Jussi Mitteli <jussi.mitteli@gmail.com>
2025-12-14 13:15:37 +0200

    updated H6b

---
+++ H6b/exampleclass.h
@@ -0,0 +1,20 @@
+#ifndef EXAMPLECLASS_H
+#define EXAMPLECLASS_H
+#include <QThread>
+#include <QObject>
+#include <QCoreApplication>
+#include <QDebug>
+
+class ExampleClass : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ExampleClass(QObject *parent = nullptr);
+    void startToWait();
+signals:
+    void readyToSay();
+public slots:
+    void sayHelloSlot();
+};
+
+#endif // EXAMPLECLASS_H
